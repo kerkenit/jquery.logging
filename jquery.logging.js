9a36b3b843aed1fe6a66b6e8cd252a48807770e5
Kerk en IT <kerkenit@users.noreply.github.com>
2014-01-21 10:45:40 +0000

    Added first release

---
+++ jquery.logging.js
@@ -0,0 +1,56 @@
+﻿$(function () {
+
+	$.fn.logging = function (val) {
+
+		var getType = function (val) {
+			if (typeof val === 'undefined') return 'undefined';
+			if (typeof val === 'object' && !val) return 'null';
+			return ({}).toString.call(val).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+		};
+
+		var isConsole = function () {
+			return (this.console !== undefined && this.console !== null && this.console.log !== undefined);
+		};
+
+		var isString = function (val) {
+			var type = getType(val);
+			return (type == 'string' || type == 'number' || type == 'boolean');
+		};
+
+		var getConsolVal = function (val) {
+			if (val !== undefined && val != null && getType(val)) {
+				if (isString(val)) {
+					return val;
+				}
+				else if (getType(val) == 'object') {
+					return val;
+				}
+				else {
+					return getType(val);
+				}
+			}
+			return undefined;
+		};
+
+		var getTextVal = function (val) {
+			if (val !== undefined && val != null && getType(val)) {
+				if (isString(val)) {
+					return val;
+				}
+			}
+			return undefined;
+		};
+
+		if (isConsole()) {
+			var toLog = getConsolVal(val);
+			if (toLog !== undefined) {
+				console.log(toLog);
+			}
+			else {
+				console.log('Value in undefined');
+			}
+		} else {
+			document.write(getTextVal(val));
+		};
+	};
+});
